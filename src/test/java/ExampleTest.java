a470ba2c3240dc9823a7675a0243c36e44ef6fea
agorozhanko <andrei.gorozhanko@gmail.com>
2023-02-05 15:52:13 +0300

    Лекция 2. Задание 3. Сценарий для логина в панель администрирования учебного приложения http://localhost/litecart/admin/.

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,13 +1,24 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ExampleTest extends BaseTest {
 
-    @Test(description = "Проверка заголовка сайта 'stackoverflow.com'")
-    public void verifyTitle() {
-        driver.get("https://stackoverflow.com/");
-        WebElement logo = driver.findElement(By.xpath("//a[contains(@class,'s-topbar--logo')]//span"));
-        assertEquals(logo.getText(), "Stack Overflow");
+    @BeforeClass
+    void goToUrl() {
+        driver.get("http://localhost/litecart/admin/");
+    }
+
+    @Test(description = "логина в панель администрирования учебного приложения http://localhost/litecart/admin/")
+    public void login() {
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("admin");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("admin");
+        WebElement login = driver.findElement(By.name("login"));
+        login.click();
+        WebElement logo = driver.findElement(By.xpath("//img[@title='My Store']"));
+        assertTrue(logo.isDisplayed());
     }
 }
